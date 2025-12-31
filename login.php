b0d3bdefd8e591ef5e9512b57cbcd4be40216c6a
anurajyellurkar <168811524+anurajyellurkar@users.noreply.github.com>
2025-12-31 20:58:18 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,33 @@
 <?php
-ob_start();
 session_start();
-include 'partials/header.php';
+include 'config.php';
 
-if($_POST){
-    $q=$conn->prepare("SELECT * FROM users WHERE email=? AND password=MD5(?)");
+if ($_POST) {
+    $q = $conn->prepare("SELECT * FROM users WHERE email=? AND password=MD5(?)");
     $q->bind_param("ss", $_POST['email'], $_POST['password']);
     $q->execute();
     $u = $q->get_result()->fetch_assoc();
 
-    if($u){
+    if ($u) {
         $_SESSION['user'] = $u;
         header("Location: index.php");
         exit;
     }
 
-    echo "<div class='card'>Invalid</div>";
+    $error = "Invalid email or password";
 }
 ?>
 
+<?php include 'partials/header.php'; ?>
+
 <h2>Login</h2>
+
+<?php if (!empty($error)): ?>
+<div class="card" style="color:red;">
+    <?= $error ?>
+</div>
+<?php endif; ?>
+
 <form class="card" method="POST">
     <input name="email" placeholder="Email"><br><br>
     <input type="password" name="password" placeholder="Password"><br><br>
@@ -27,5 +35,3 @@ if($_POST){
 </form>
 
 <?php include 'partials/footer.php'; ?>
-
-<?php ob_end_flush(); ?>
